af0dd2d6c8f4e333e3b938db7a21b59594d96700
garciafede105 <garciafede105@gmail.com>
2025-03-08 05:15:29 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    if (x == 1) {
+        printf("Hello, world!\n");
+    } else if (x == 2) {
+        printf("I'm a simple GitHub school project for my classmates and me to collaborate on.\n");
+    } else if (x == 3) {
+        printf("We can use this to practice writing code in C and learn new programming concepts.\n");
+    } else if (x == 4) {
+        printf("I hope you enjoy it! Let me know if you have any questions.\n");
+    } else if (x == 5) {
+        printf("Thank you for using our GitHub school project!\n");
+    }
+    return 0;
+}
