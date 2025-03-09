c7224ed031f9428fe7a17bed098d935c7753e638
garciafede105 <garciafede105@gmail.com>
2025-03-09 07:10:00 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int x = 10;
+    if (x > 5) {
+        printf("The value of x is greater than 5\n");
+    } else {
+        printf("The value of x is less than or equal to 5\n");
+    }
+    return 0;
+}
